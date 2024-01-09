c2448c5a6c9175e09749eedc6088ede8960468c4
savvyh <7277@holbertonstudents.com>
2024-01-09 14:21:47 +0000

    Add a function that checks if a string is a numeric value

---
+++ is_numeric.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * is_numeric - Checks if a string is a numeric value
+ * @str: The string to check
+ * Return: 1 if numeric, 0 otherwise
+ */
+int is_numeric(const char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+
+	if (str[0] == '-')
+		i++;
+
+	for (; str[i]; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
